58fac7216fed2a9f16549ccc923ab8aee72b36dc
Eduardo Rampon Meireles <37454606+edu-rm@users.noreply.github.com>
2021-10-01 11:14:34 -0300

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import { Container } from './styles';
+import SideBar from '../../components/SideBar';
 
 function Home() {
   return (
+    <>
+      <SideBar />
       <Container>
-          <h1>Helloword</h1>
+        <h1>IFC Videira - Métodos numéricos 2021.2</h1>
       </Container>
+     </>
   );
 }
 
