aeb7b9ea32b1ba56dd7c41f0c47c782bb64af09c
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-03 20:31:50 -0300

    Revert "Merge pull request #2 from numerical-problems/sidebar-starter"
    This reverts commit 5a65c48014c6d6651d08cb93719a203b8d3d7c16, reversing
    changes made to 29e3e5d1dee30af01b91eaaf21d85ab771310e36.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import { Container } from './styles';
-import SideBar from '../../components/SideBar';
 
 function Home() {
   return (
-    <>
-      <SideBar />
       <Container>
-        <h1>IFC Videira - Métodos numéricos 2021.2</h1>
+          <h1>Helloword</h1>
       </Container>
-     </>
   );
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,6 +2,4 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     background: red;
-    max-width: 1200px;
-    margin: 0 auto;
 `;
